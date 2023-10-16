cba613328144bf3393acc020a159956f747565ae
LucasLimazz <lucaslima15215@gmail.com>
2023-10-16 13:22:24 -0300

    Start Commit

---
+++ app/src/main/java/com/lucas/copodagua/GlassPreferences.kt
@@ -0,0 +1,28 @@
+package com.lucas.copodagua
+
+import android.content.Context
+import androidx.core.content.edit
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class GlassPreferences(private val context: Context) {
+
+    companion object {
+        const val PREFS_NAME = "com.lucas.copodagua.preferences"
+        const val PREFS_PREFIX = "key_"
+    }
+
+    private val sdf = SimpleDateFormat("ddMMyyyy", Locale.US)
+
+    fun save(value: Int) {
+        println("teste $value")
+        context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).edit {
+            putInt(PREFS_PREFIX + sdf.format(Date()), value)
+        }
+    }
+
+    fun fetch() = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+            .getInt(PREFS_PREFIX + sdf.format(Date()), 0)
+
+}
---
+++ app/src/main/java/com/lucas/copodagua/GlassType.kt
@@ -0,0 +1,7 @@
+package com.lucas.copodagua
+
+enum class GlassType(val value: Int) {
+    SMALL(200),
+    MEDIUM(350),
+    LARGE(500)
+}
